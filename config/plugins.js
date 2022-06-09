fe49dd0d27edb9ad3c54c64833c44043205f033d
Emre Simsek <emresimseka@gmail.com>
2022-06-09 09:50:34 +0300

    FEAT: plugins

--- config/plugins.js
+++ config/plugins.js
@@ -6,6 +6,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
+
         api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
